28ce5aac285e8198859d3f9e9d388d15aa740ebd
EdoWahdana <wahdanaedo@gmail.com>
2023-05-14 22:13:16 +0700

    formatting

--- components/cockponents/Footer/index.tsx
+++ components/cockponents/Footer/index.tsx
@@ -3,20 +3,24 @@ import Logo from "../Logo";
 import SocialMedia from "../SocialMedia";
 
 const Footer = () => {
-    return <footer className="bg-gray-100 " >
-        <Container>
-            <div className="text-white pt-10 pb-5 flex flex-col items-center">
-                <div className="flex flex-col items-center ">
-                    <Logo width={170} height={70} />
-                    <div className="text-black mt-8">
-                        <SocialMedia />
-                    </div>
-                </div>
-                <hr className="bg-gray-300 border-0 h-px w-full my-5" />
-                <p className="text-gray-500" >&copy; 2023 Cockright reserved to Cockmaster by Cocklabs</p>
+  return (
+    <footer className="bg-gray-100 ">
+      <Container>
+        <div className="text-white pt-10 pb-5 flex flex-col items-center">
+          <div className="flex flex-col items-center ">
+            <Logo width={170} height={70} />
+            <div className="text-black mt-8">
+              <SocialMedia />
             </div>
-        </Container>
+          </div>
+          <hr className="bg-gray-300 border-0 h-px w-full my-5" />
+          <p className="text-gray-500">
+            &copy; 2023 Cockright reserved to Cockmaster by Cocklabs
+          </p>
+        </div>
+      </Container>
     </footer>
-}
+  );
+};
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,36 +11,36 @@ import Roadmap from "../components/cockponents/Roadmap";
 
 
 const Template2: NextPage = () => {
-    return (
-      <div>
-        <Head>
-          <title>Cockmaster</title>
-          <meta name="description" content="This NFT Landing Page" />
-          <link rel="Cock Icon" href="fullbody.png" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <NextSeo title="Cockmaster" description="Cockmaster NFT Collection" />
-        <div id="home">
-          <div className="bg-[url('../public/grass.png')]">
-            <Navbar />
-          </div>
-          <Header />
-          <div className="gradient_header_tem1">
-            <Aboutus />
-          </div>
-          <Roadmap />
-          <div className="bg-[url('../public/grass.png')] bg-[center_bottom_100px]">
-            <FQA />
-            <Footer />
-          </div>
+  return (
+    <div>
+      <Head>
+        <title>Cockmaster</title>
+        <meta name="description" content="This NFT Landing Page" />
+        <link rel="Cock Icon" href="fullbody.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <NextSeo title="Cockmaster" description="Cockmaster NFT Collection" />
+      <div id="home">
+        <div className="bg-[url('../public/grass.png')]">
+          <Navbar />
+        </div>
+        <Header />
+        <div className="gradient_header_tem1">
+          <Aboutus />
+        </div>
+        <Roadmap />
+        <div className="bg-[url('../public/grass.png')] bg-[center_bottom_100px]">
+          <FQA />
+          <Footer />
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Template2;
